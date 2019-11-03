4ca63b6cb702f0635959abdb63a288d194ce5933
lukemcd9 <lukemcd97@gmail.com>
2019-11-02 20:20:08 -1000

    Finished landing page mockup finished in 10m 52s

--- app/imports/ui/components/Footer.jsx
+++ app/imports/ui/components/Footer.jsx
@@ -3,11 +3,11 @@ import React from 'react';
 /** The Footer appears at the bottom of every page. Rendered by the App Layout component. */
 class Footer extends React.Component {
   render() {
-    const divStyle = { paddingTop: '15px' };
+    const divStyle = { paddingTop: '15px', color: 'white' };
     return (
         <footer>
           <div style={divStyle} className="ui center aligned container">
-            <hr />
+            <hr style={{ color: 'white' }}/>
               Department of Information and Computer Sciences <br />
               University of Hawaii<br />
               Honolulu, HI 96822 <br />
